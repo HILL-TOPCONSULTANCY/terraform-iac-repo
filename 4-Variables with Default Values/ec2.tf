242fead2b0553eea9de4aabbdedc40d2d97cc956
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-09-28 19:29:54 +0200

    Update ec2.tf

--- 4-Variables with Default Values/ec2.tf
+++ 4-Variables with Default Values/ec2.tf
@@ -1,8 +1,8 @@
 # Below is the resource block which creates EC2 Instance
-resource "aws_instance" "prod-instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+resource "aws_instance" "instance" {
+  ami           = 
+  instance_type = 
   tags = {
-    Name = var.ec2_name_tag
+    Name = 
   }
 }
